fe0c678269f388b9e6ca4ac283b47bb273d9d645
nesma manasarh <52491098+anaal@users.noreply.github.com>
2022-07-11 16:55:13 +0300

    edit html file and add my files

---
+++ script.js
@@ -0,0 +1,45 @@
+function buildNewTodo(value) {
+    const todoList = document.querySelector('#todo-list ul');
+    // create li element 
+    const parentLi = document.createElement('li');
+
+    // create todo title 
+    const todoSpanTitle = document.createElement('span');
+    todoSpanTitle.textContent = value;
+    const randomId = Math.floor(Math.random() * (100000 - 1 + 1) + 1);
+    parentLi.setAttribute('id', randomId);
+    // create delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+    deleteButton.classList.add('actions');
+    deleteButton.addEventListener('click', () => {
+        deleteTodo(randomId);
+    });
+    // create edit button
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    editButton.classList.add('actions');
+    // add title and delete and edit to li
+    parentLi.append(todoSpanTitle, deleteButton, editButton);
+    todoList.append(parentLi);
+}
+
+function deleteTodo(randomId) {
+    const todoItem1 = document.getElementById(`${randomId}`);
+    todoItem1.remove();
+}
+
+function addNewTodo() {
+    const todoItem = document.getElementById('todo-item');
+    const warning = document.getElementById('warning');
+    const todoItemValue = todoItem.value;
+    if (!todoItemValue.length) {
+        warning.classList.remove('hidden-item');
+        return;
+    }
+    if (!warning.classList.contains('hidden-item')) {
+        warning.classList.add('hidden-item');
+    }
+    buildNewTodo(todoItemValue);
+    todoItem.value = '';
+}
